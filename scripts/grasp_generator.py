f732b5ae65ffe05d90b6004e40e043ac2b6bc943
Sophie Lueth <sophie.lueth@me.com>
2023-05-16 19:09:05 +0200

    working grasper v0.9

--- scripts/grasp_generator.py
+++ scripts/grasp_generator.py
@@ -3,9 +3,11 @@ import argparse
 import rospy
 # import geometry_msgs
 from sensor_msgs.msg import PointCloud2, Image
+from geometry_msgs.msg import PoseArray, Pose
 # import sensor_msgs.point_cloud2 as pc2
 # from std_msgs.msg import Header
 from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
+from tf.transformations import euler_from_quaternion, quaternion_from_euler, quaternion_from_matrix
 import ros_numpy
 # import tf
 import tf2_ros
@@ -25,8 +27,16 @@ from vgn.simulation import ClutterRemovalSim
 # SCENE_SIZE = 0.3 # size of the scene in meters
 
 class GraspGenerator:
-	def __init__(self, camera_type='zed', net_type='neu_grasp_pn_deeper', net_path=None, tsdf_res=64, scene_size=0.3, downsampl_size=0.005):
+	def __init__(self, grasp_frame_name='grasp_origin', grasp_topic_name="/generated_grasps", camera_type='zed',
+	      		 net_type='neu_grasp_pn_deeper', net_path=None, tsdf_res=64, scene_size=0.3, downsampl_size=0.005):
 		
+		# Publish grasps to topic
+		self.grasp_topic_name = grasp_topic_name
+		self.grasp_pub = rospy.Publisher(self.grasp_topic_name, PoseArray, queue_size=1, latch=False)
+
+		# grasps fill be in this frame
+		self.grasp_frame_name = grasp_frame_name
+
 		# Setup camera
 		self.camera_type = camera_type
 		if self.camera_type == 'zed':
@@ -80,9 +90,9 @@ class GraspGenerator:
 		while not rospy.is_shutdown():
 			try:
 				if self.camera_type == 'zed':
-					table_to_pcl_tf = self.tf_buffer.lookup_transform('grasp_origin', 'zed2_left_camera_frame', rospy.Time(0))
+					table_to_pcl_tf = self.tf_buffer.lookup_transform(self.grasp_frame_name, 'zed2_left_camera_frame', rospy.Time(0))
 				elif self.camera_type == 'xtion':
-					table_to_pcl_tf = self.tf_buffer.lookup_transform('grasp_origin', 'xtion_rgb_optical_frame', rospy.Time(0))				
+					table_to_pcl_tf = self.tf_buffer.lookup_transform(self.grasp_frame_name, 'xtion_rgb_optical_frame', rospy.Time(0))				
 			except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
 				print("[WARNING: Could not find some TFs. Check TF and obj names]")
 				# print(e)
@@ -116,9 +126,9 @@ class GraspGenerator:
 		while not rospy.is_shutdown():
 			try:
 				if self.camera_type == 'zed':
-					cam_to_table_tf = self.tf_buffer.lookup_transform('zed2_left_camera_optical_frame', 'grasp_origin', rospy.Time(0))
+					cam_to_table_tf = self.tf_buffer.lookup_transform('zed2_left_camera_optical_frame', self.grasp_frame_name, rospy.Time(0))
 				elif self.camera_type == 'xtion':
-					cam_to_table_tf = self.tf_buffer.lookup_transform('xtion_rgb_optical_frame', 'grasp_origin', rospy.Time(0))
+					cam_to_table_tf = self.tf_buffer.lookup_transform('xtion_rgb_optical_frame', self.grasp_frame_name, rospy.Time(0))
 			except:
 				print("[WARNING: Could not find some TFs. Check TF and obj names]")
 				# rate.sleep()
@@ -172,9 +182,17 @@ class GraspGenerator:
 		if visualize:
 			# Running viz of the scene point clouds and meshes
 			o3d_vis = o3d.visualization.Visualizer()
-			o3d_vis.create_window()
+			o3d_vis.create_window(width=1863, height=1052)
 			state.pc.colors = o3d.utility.Vector3dVector(np.tile(np.array([0, 0, 0]), (np.asarray(state.pc.points).shape[0], 1)))
 			o3d_vis.add_geometry(state.pc, reset_bounding_box=True)
+			
+			# Reset view
+			ctr = o3d_vis.get_view_control()
+			parameters = o3d.io.read_pinhole_camera_parameters("/neugraspnet/neugraspnet_repo/o3d_vis_screen_camera_params2.json")
+			ctr.convert_from_pinhole_camera_parameters(parameters)
+			for i in range(50):
+				o3d_vis.poll_events()
+				o3d_vis.update_renderer()
 		else:
 			o3d_vis = None
         
@@ -191,11 +209,39 @@ class GraspGenerator:
 			grasp_tf = Transform(grasp_frame_rot, grasp_center).as_matrix()
 			grasps_final.append(grasp_tf)
 	
-		return grasps_final, scores
+		return grasps_final, scores, o3d_vis
 
-# # Run this as a script to test
-# rospy.init_node('grasp_objs')
-# grasp_gen = GraspGenerator(camera_type='zed')
-# grasp, scores = grasp_gen.get_grasps(visualize=True)
-# rospy.spin()
-# # We get back grasps and scores and can then call the appropriate tiago dual pick place function that can handle multiple grasps
+# # Run this as a script
+rospy.init_node('grasp_generator')
+grasp_gen = GraspGenerator(camera_type='zed')
+grasps, scores, o3d_vis = grasp_gen.get_grasps(visualize=True)
+if len(grasps) > 0:
+	print("Found {} grasps with scores: {}".format(len(grasps), scores))
+	# We publish the grasps to the appropriate topic
+	rospy.loginfo("Publishing grasps to topic: {}".format(grasp_gen.grasp_topic_name))
+	# Make grasp pose array message
+	grasp_pose_array = PoseArray()
+	grasp_pose_array.header.frame_id = grasp_gen.grasp_frame_name
+	grasp_pose_array.header.stamp = rospy.Time.now()
+	grasp_pose_array.poses = [Pose() for _ in range(len(grasps))]
+	for i, grasp in enumerate(grasps):
+		grasp_pose_array.poses[i].position.x = grasp[0,3]
+		grasp_pose_array.poses[i].position.y = grasp[1,3]
+		grasp_pose_array.poses[i].position.z = grasp[2,3]
+		rot_quatt = quaternion_from_matrix(grasp)
+		grasp_pose_array.poses[i].orientation.x = rot_quatt[0]
+		grasp_pose_array.poses[i].orientation.y = rot_quatt[1]
+		grasp_pose_array.poses[i].orientation.z = rot_quatt[2]
+		grasp_pose_array.poses[i].orientation.w = rot_quatt[3]
+	
+	# Publish
+	grasp_gen.grasp_pub.publish(grasp_pose_array)
+
+	if o3d_vis:
+		# Run viz
+		while not rospy.is_shutdown():
+			o3d_vis.poll_events()
+			o3d_vis.update_renderer()
+	rospy.spin()
+else:
+	print("[No grasps found]")
---
+++ scripts/grasper.py
@@ -0,0 +1,187 @@
+# from pathlib import Path
+# import argparse
+import rospy
+from geometry_msgs.msg import PoseArray, Pose, PoseStamped
+from sensor_msgs.msg import PointCloud2
+from std_srvs.srv import Empty, EmptyRequest
+from sensor_msgs import point_cloud2
+from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
+import ros_numpy
+# import tf
+import tf2_ros
+import numpy as np
+# from std_msgs.msg import Header
+
+# tiago dual pick place
+from tiago_dual_pick_place.srv import PickPlaceSimple
+from moveit_msgs.msg import MoveItErrorCodes
+from tf.transformations import euler_from_quaternion, quaternion_from_euler, quaternion_from_matrix
+
+# Reads the generated grasps and sends them to the pick place service. Also activates and deactivates mapping
+
+class Grasper:
+	def __init__(self, grasps_sub_topic="/generated_grasps", grasp_pub_topic="/grasp/pose", grasp_frame_name='grasp_origin',
+	      		camera_type='zed', octomap_topic_name="/filtered_points_for_mapping", filter_pcl=True, arm="right"):
+		
+		self.grasps_sub_topic = grasps_sub_topic
+
+		self.grasp_pub_topic = grasp_pub_topic
+		self.grasp_pub = rospy.Publisher(self.grasp_pub_topic, PoseStamped, queue_size=1, latch=True)
+
+		self.grasp_frame_name = grasp_frame_name
+		# tf listener to get the point cloud in the correct frame
+		self.tf_buffer = tf2_ros.Buffer()
+		self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+
+		# To control when moveit should and should not use the camera depth,
+		# we set up a subscriber and publisher that will forward information to the moveit octomap server
+		self.camera_type = camera_type
+		if self.camera_type == 'zed':
+			# get the pointcloud from the topic
+			self.pcl_topic_name = "/zed2/zed_node/point_cloud/cloud_registered"
+		elif self.camera_type == 'xtion':
+			self.pcl_topic_name = "/xtion/depth_registered/points"
+		else:
+			raise ValueError('Invalid camera type. Choose either zed or xtion.')
+		# start the subscriber with an explicit call
+		# self.pcl_sub = rospy.Subscriber(self.pcl_topic_name, PointCloud2, self.forward_pcl, queue_size=1)
+
+		# We can optionally filter the point cloud to remove points very close to the grasp
+		self.filter_pcl = filter_pcl
+		self.filter_radius = 0.07 # 7cm
+		
+		self.octomap_topic_name = octomap_topic_name
+		self.octomap_pub = rospy.Publisher(self.octomap_topic_name, PointCloud2, queue_size=1, latch=True)
+
+		self.clear_octomap_srv = rospy.ServiceProxy('/clear_octomap', Empty)
+		self.clear_octomap_srv.wait_for_service()
+		rospy.loginfo("[Connected to octomap clear service]")
+		
+		self.current_arm = arm # 'left' or 'right'
+
+		self.grasp_pose_array = None
+		self.current_grasp_pose = None
+
+	def forward_pcl(self, ros_point_cloud):
+		# forward the point cloud to the moveit octomap server topic (defined in the sensor yaml file)
+
+		# Optional: filter the point cloud to remove points very close to the grasp
+		if self.filter_pcl:
+			assert self.current_grasp_pose is not None, "No current grasp pose set. Cannot filter point cloud."
+
+			# transform the point cloud to the grasp_origin frame
+			while not rospy.is_shutdown():
+				try:
+					if self.camera_type == 'zed':
+						table_to_pcl_tf = self.tf_buffer.lookup_transform(self.grasp_frame_name, 'zed2_left_camera_frame', rospy.Time(0))
+					elif self.camera_type == 'xtion':
+						table_to_pcl_tf = self.tf_buffer.lookup_transform(self.grasp_frame_name, 'xtion_rgb_optical_frame', rospy.Time(0))				
+				except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+					print("[WARNING: Could not find some TFs. Check TF and obj names]")
+					# print(e)
+					# rate.sleep()
+					continue
+				break
+
+			# transform the point cloud to the grasp_origin frame
+			cloud_transformed = do_transform_cloud(ros_point_cloud, table_to_pcl_tf)
+			# convert to numpy array
+			pc = ros_numpy.numpify(cloud_transformed)
+			points=np.zeros((pc.shape[0],3))
+			points[:,0]=pc['x']
+			points[:,1]=pc['y']
+			points[:,2]=pc['z']
+			# remove nans
+			points = points[~np.isnan(points).any(axis=1)]
+			# crop point cloud?
+			# points = points[(points[:,0] > 0) & (points[:,0] < self.size) & (points[:,1] > 0) & (points[:,1] < self.size) & (points[:,2] > 0) & (points[:,2] < self.size)]
+			# remove points very close to the grasp
+			curr_grasp_center = np.array([self.current_grasp_pose.position.x, self.current_grasp_pose.position.y, self.current_grasp_pose.position.z])
+			# print("Points before filtering: ", points.shape)
+			points = points[np.linalg.norm(points - curr_grasp_center, axis=1) > self.filter_radius]
+			# print("Points after filtering: ", points.shape)
+			
+			# convert back to ros point cloud		
+			ros_point_cloud.header.stamp = rospy.Time.now()
+			ros_point_cloud.header.frame_id = self.grasp_frame_name
+			ros_point_cloud = point_cloud2.create_cloud(ros_point_cloud.header, ros_point_cloud.fields[0:3], points) # Using only xyz (no rgb)
+
+		self.octomap_pub.publish(ros_point_cloud)
+	
+	def stop_mapping(self):
+		# stop forwarding the point cloud to the moveit octomap server topic
+		self.pcl_sub.unregister()
+	
+	def start_mapping(self):
+		# Clear existing octomap
+		rospy.loginfo("Clearing octomap")
+		self.clear_octomap_srv.call(EmptyRequest())
+		# start forwarding the point cloud to the moveit octomap server topic
+		self.pcl_sub = rospy.Subscriber(self.pcl_topic_name, PointCloud2, self.forward_pcl, queue_size=1)
+
+
+# Run as a script
+rospy.init_node('grasper_node')
+
+grasper = Grasper()
+
+# Get grasps from the grasp generator
+rospy.loginfo("[Waiting for grasps published on topic: %s...]", grasper.grasps_sub_topic)
+grasper.grasp_pose_array = rospy.wait_for_message(grasper.grasps_sub_topic, PoseArray, timeout=300)
+
+rospy.loginfo("[Received %d grasp poses]",len(grasper.grasp_pose_array.poses))
+
+# We assume the grasp poses are sorted by score, so we start from the topic
+for i, grasp_pose in enumerate(grasper.grasp_pose_array.poses):
+	rospy.loginfo("[Trying grasp %d/%d]", i+1, len(grasper.grasp_pose_array.poses))
+	# Set current in the grasper object
+	grasper.current_grasp_pose = grasp_pose
+	# Start mapping with filtering enabled. This should filter out the points very close to the CURRENT grasp
+	grasper.start_mapping()
+	rospy.sleep(1.5)
+	grasper.stop_mapping()
+	# publish this grasp pose to the topic that pick-place pipeline expects
+	grasp_pub_msg = PoseStamped()
+	grasp_pub_msg.header.stamp = rospy.Time.now()
+	grasp_pub_msg.header.frame_id = grasper.grasp_frame_name
+	grasp_pub_msg.pose = grasp_pose
+	grasper.grasp_pub.publish(grasp_pub_msg)
+
+	# Now call the pick service and wait for the result
+	rospy.loginfo("[Calling pick service...]")
+	pick_result = rospy.ServiceProxy('/pick', PickPlaceSimple)(grasper.current_arm)
+
+	# Check if the pick was successful
+	if pick_result.error_code == MoveItErrorCodes.SUCCESS:
+		rospy.loginfo("[Pick successful!]")
+		# If so, we can stop here
+		break
+	else:
+		rospy.loginfo("[Pick failed. Trying next grasp pose...]")
+		# If not, we try the next grasp pose
+		continue
+
+
+# # Testtttt
+# grasper.start_mapping()
+# rospy.sleep(1)
+# grasper.stop_mapping()
+# grasps, scores = grasper.grasp_gen.get_grasps(visualize=True)
+# # Send grasp command to moveit pick and place pipeline
+# grasp_pose_pub = rospy.Publisher('/grasp/pose', geometry_msgs.msg.PoseStamped, queue_size=1, latch=True)
+# grasp_pose_msg = geometry_msgs.msg.PoseStamped()
+# grasp_pose_msg.header.stamp = rospy.Time.now()
+# grasp_pose_msg.header.frame_id = 'base_footprint'
+# grasp_pose_msg.pose.position.x = 0.5
+# grasp_pose_msg.pose.position.y = -0.5
+# grasp_pose_msg.pose.position.z = 0.75
+# grasp_pose_msg.pose.orientation.x = 0.0
+# grasp_pose_msg.pose.orientation.y = 0.0
+# grasp_pose_msg.pose.orientation.z = 0.0
+# grasp_pose_msg.pose.orientation.w = 1.0
+# grasp_pose_pub.publish(grasp_pose_msg)
+# rospy.loginfo("[Sending /pick command...]")
+# # call /pick rosservice
+# grasper.current_arm = 'left'
+# pick_result = rospy.ServiceProxy('/pick', PickPlaceSimple)(grasper.current_arm)
+# rospy.spin()
